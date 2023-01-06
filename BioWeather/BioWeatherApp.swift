1d34a58fc7991c6a0d8738539918edeafe827424
Rafael Lima <rafael.lima2403@gmail.com>
2023-01-06 15:04:20 +0000

    Create Project

---
+++ BioWeather/BioWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioWeatherApp.swift
+//  BioWeather
+//
+//  Created by Rafael Lima on 06/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BioWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
