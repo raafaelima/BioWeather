4953adb7642ad0f435dc85767ab6ca7b090cf50f
Rafael Lima <rafael.lima2403@gmail.com>
2023-01-06 19:10:54 +0000

    Create Authentication View

---
+++ BioWeather/Authentication/AuthenticationView.swift
@@ -0,0 +1,72 @@
+//
+//  AuthenticationView.swift
+//  BioWeather
+//
+//  Created by Rafael Lima on 06/01/2023.
+//
+
+import SwiftUI
+import LocalAuthentication
+
+struct AuthenticationView: View {
+
+    @State var userName = ""
+
+    var body: some View {
+        ZStack {
+            LinearGradient(colors: [.gradientStart, .gradientEnd], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+
+            VStack(spacing: 20) {
+               welcome()
+                userNameField()
+                findMyWeatherInfo()
+            }
+            .padding()
+        }
+    }
+
+    private func welcome() -> some View {
+        VStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+
+            Text("Welcome to BioWeather")
+                .font(.title2)
+        }
+        .padding(.bottom, 30)
+    }
+
+    private func userNameField() -> some View {
+        HStack {
+            Image(systemName: "person.crop.circle")
+            TextField("Username", text: $userName)
+        }
+        .padding()
+        .background(.gray.opacity(0.2))
+        .cornerRadius(15)
+        .padding(.horizontal, 20)
+    }
+
+    private func findMyWeatherInfo() -> some View {
+        Button(action: {}, label: {
+            Text("Find My Weather Info")
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 6)
+        })
+        .padding(.horizontal, 50)
+        .padding(.top, 20)
+        .buttonStyle(.borderedProminent)
+        .disabled(userName.isEmpty)
+    }
+}
+
+#if DEBUG
+struct AuthenticationView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthenticationView()
+    }
+}
+#endif
---
+++ BioWeather/Extensions/Color+Custom.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Custom.swift
+//  BioWeather
+//
+//  Created by Rafael Lima on 06/01/2023.
+//
+
+import SwiftUI
+import Foundation
+
+extension Color {
+    static let gradientStart = Color("gradientStart")
+    static let gradientEnd = Color("gradientEnd")
+}
